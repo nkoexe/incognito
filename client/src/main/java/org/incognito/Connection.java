355b2a0c147799812bff861962ceae13a50414fb
Tahsan J Jilan <jilan.tahsan21@gmail.com>
2025-07-08 00:28:37 +0200

    default host name change

--- client/src/main/java/org/incognito/Connection.java
+++ client/src/main/java/org/incognito/Connection.java
@@ -8,7 +8,7 @@ import java.net.UnknownHostException;
 public class Connection {
     private static Logger logger = Logger.getLogger(Connection.class.getName());
 
-    private String host = "localhost";
+    private String host = "incognito.njco.dev";
     private int port = 58239;
 
     private Socket socket;
